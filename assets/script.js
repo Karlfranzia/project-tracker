56082293acf1f9127eeb20ce70b8ccb8d3ebddbf
Jegaco <jeffgcorpuz@gmail.com>
2023-01-26 21:07:00 -0800

    Started project tracker card and modal dialog

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// var myModal = document.getElementById('modal')
+// var myInput = document.getElementById('modal')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
+
+
+$( function() {
+    $("#datepicker").datepicker();
+    changeMonth: true;
+    changeYear: true;
+  } );
